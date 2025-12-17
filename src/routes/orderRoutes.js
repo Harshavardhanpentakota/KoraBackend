a1de6e02e059da19fd96e23c3c0cfc7aa12d25e7
sreesha <sreesha0205@gmail.com>
2025-12-18 04:33:40 +0530

    commit

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -21,13 +21,13 @@ router.post('/', validate(orderValidation), createOrder);
 router.put('/:id', updateOrder);
 router.get('/', getOrders);
 router.get('/status/:id', getOrderStatus);
+router.get('/:id', getOrder); // Made public for customer order access
 router.post('/:id/payment/create', createPayment);
 router.put('/:id/pay', processOrderPayment);
 router.delete('/:id', cancelOrder);
 
 // Protected routes
 router.get('/table/:tableId', protect, authorize('admin', 'cashier', 'waiter'), getOrderByTable);
-router.get('/:id', protect, authorize('admin', 'cashier', 'kitchen'), getOrder);
 router.put('/:id/status', protect, authorize('admin', 'cashier'), updateOrderStatus);
 
 module.exports = router;
